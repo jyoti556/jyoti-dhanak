fb920ccfc7d7fda3f1028a6d5af37bb2a5faf972
jyoti556 <dhanakjyoti1@gmail.com>
2022-01-23 16:29:09 +0530

    first commit

---
+++ multiple.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class multiple {
+    public static void main(String[] args){
+        Scanner jv=new Scanner(System.in);
+        // float v
+        // float k=((v*v)-(u*u))/(2*a*s);
+        for (int i=0;i<16;i++){
+            if (i%2==0 && i%3==0)
+            System.out.println("no");
+            else
+            System.out.println(i);
+        }
+        
+
+    }
+    
+}
