6899b7be7e696cff564b7e5a95a9d9b586bd528c
jyoti556 <84786105+jyoti556@users.noreply.github.com>
2022-04-24 10:19:00 +0530

    Add files via upload

---
+++ babes.cpp
@@ -0,0 +1,87 @@
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     .length
+
+// }
+// // int count(int n){
+// // int k=1;
+// // for(int i=1;i<=n;++i){
+// //     if (i%2==0){
+// //         k=k+1;
+// //         // cout<<k;
+// //     }
+// //     else if(i%2!=0){
+// //         k=2*(k);
+// //         // cout<<k;
+        
+    
+// //     }
+// //     }
+// //     return(k);
+
+
+// // }
+// // int main(){
+// //     int n,t,i;
+// //     cout<<("no.of test cases");
+// //     cin>>t;
+// //     for(i=0;i<=t;++i){
+// //         cin>>n;
+// //         cout<<count(n);
+// //         }
+// // }
+    
+// // //     if (i%2==0){
+// // //         k=k+1;
+// // //         // cout<<k;
+// // //     }
+// // //     else if(i%2!=0){
+// // //         k=2*(k);
+// // //         // cout<<k;
+        
+    
+// // //     }
+// // //     }
+// // //     cout<<k;
+// // // }
+
+// // // // int main(){
+// // // // // int n;
+// // // // // cout<<("enter the size of array");
+// // // // // cin>>n;
+// // // // int arr1[26],l,k;
+// // // // string str;
+
+// // // // for (int i = 0; i < 26; ++i) {
+// // // //         cin >>(arr1[i]);
+// // // //         }
+
+// // // for (int j = 0; j < 26; ++j) {
+// // //         cout <<arr1[j] << "  ";
+// // //         }
+// // // }
+     
+// // // int i;
+// // // int max = arr1[0];
+// // //  for (i = 1; i < n; i++){
+// // //         if (arr1[i] > max){
+// // //             max = arr1[i];
+           
+// // //            } 
+// // //  }cout<<max;
+// // //     cout<<"enter l";
+// // //     cin>>l;
+// // //     int m =max-l;
+// // //     if(m>0)
+// // //         cout<<m;
+// // //         else if(m<=0)
+// // //         cout<<"0";
+    
+// // // }
+
+ 
+
+
+
